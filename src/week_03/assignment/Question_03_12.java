0574dd03a808a6dfe69fcf07a9512b540013a893
mssyr07 <mssyr07@gmail.com>
2023-08-23 14:18:51 +0300

    updated

--- src/week_03/assignment/Question_03_12.java
+++ src/week_03/assignment/Question_03_12.java
@@ -1,7 +1,23 @@
 package week_03.assignment;
 
+import java.util.Scanner;
+
 public class Question_03_12 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a three-digit integer : ");
+        int number1 = input.nextInt();
+
+        int temp = number1 % 10;
+        int temp2 = number1 / 100;
+
+        if (temp == temp2) {
+            System.out.println(number1 + " is a palindrome ");
+        } else {
+            System.out.println(number1 + " is not a palindrome ");
+        }
+
+
     }
 }
