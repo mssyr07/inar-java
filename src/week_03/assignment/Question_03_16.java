4c4ef07f8ba76cfbdac59631b263092adddeb5ba
mssyr07 <mssyr07@gmail.com>
2023-08-24 13:18:28 +0300

    updated

--- src/week_03/assignment/Question_03_16.java
+++ src/week_03/assignment/Question_03_16.java
@@ -1,5 +1,4 @@
 package week_03.assignment;
-
 import java.util.Scanner;
 
 public class Question_03_16 {
@@ -7,6 +6,10 @@ public class Question_03_16 {
         Scanner input = new Scanner(System.in);
 
 
+        int randomWidth = (int) (Math.random() * 201) - 100;
+        int randomHeight = (int) (Math.random() * 301) - 100;
+
+        System.out.println("Random coordinate in rectangle centered at ( 0 , 0 ) with width 100 and height 200 : " + " ( " + randomWidth + " , " + randomHeight + " ) ");
 
     }
 }
