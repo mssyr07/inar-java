aab8bdd830dbaf520af517cf748c6b75b9fbf2bf
mssyr07 <mssyr07@gmail.com>
2023-08-25 15:21:09 +0300

    updated

---
+++ src/week_03/assignment/Question_03_24.java
@@ -0,0 +1,76 @@
+package week_03.assignment;
+
+import java.util.Scanner;
+
+public class Question_03_24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int group = (int) (Math.random() * 3 + 1);
+        String groupName = null;
+
+        switch (group) {
+            case 1:
+                groupName = "Hearts";
+                break;
+            case 2:
+                groupName = "Spades";
+                break;
+            case 3:
+                groupName = "Squares";
+                break;
+            case 4:
+                groupName = "Spades";
+                break;
+        }
+
+        int number = (int) (Math.random() * 12 + 1);
+        String cardNumber = null;
+
+        switch (number) {
+            case 1:
+
+                cardNumber = "Ace";
+                break;
+            case 2:
+                cardNumber = "King";
+                break;
+            case 3:
+                cardNumber = "Queen";
+                break;
+            case 4:
+                cardNumber = "Jack";
+                break;
+            case 5:
+                cardNumber = "2";
+                break;
+            case 6:
+                cardNumber = "3";
+                break;
+            case 7:
+                cardNumber = "4";
+                break;
+            case 8:
+                cardNumber = "5";
+                break;
+            case 9:
+                cardNumber = "6";
+                break;
+            case 10:
+                cardNumber = "7";
+                break;
+            case 11:
+                cardNumber = "8";
+                break;
+            case 12:
+                cardNumber = "9";
+                break;
+            case 13:
+                cardNumber = "10";
+                break;
+        }
+
+        System.out.println("The card you picked is " + cardNumber + " of " + groupName);
+
+    }
+}
