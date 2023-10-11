3f9e14a27b8534897e168a81c47044bd838d9760
mssyr07 <mssyr07@gmail.com>
2023-10-11 15:06:19 +0300

    updated

---
+++ src/week_07/assignments/Question_07_09.java
@@ -0,0 +1,29 @@
+package week_07.assignments;
+
+import java.util.Scanner;
+
+public class Question_07_09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[] numbers = new double[10];
+
+        System.out.print("Enter teh numbers : ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextDouble();
+        }
+        System.out.println("The minimum number is " + min(numbers));
+
+    }
+
+    public static double min(double[] array) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
