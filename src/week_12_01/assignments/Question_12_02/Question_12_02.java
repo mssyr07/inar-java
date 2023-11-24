d33cbdf9e284a4a49cee5039da26ba3ef4d8d309
mssyr07 <mssyr07@gmail.com>
2023-11-24 16:52:22 +0300

    updated

---
+++ src/week_12_01/assignments/Question_12_02/Question_12_02.java
@@ -0,0 +1,7 @@
+package week_12_01.assignments.Question_12_02;
+
+public class Question_12_02 {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/week_12_01/live_class/Example_01.java
+++ src/week_12_01/live_class/Example_01.java
@@ -2,6 +2,6 @@ package week_12_01.live_class;
 
 public class Example_01 {
     public static void main(String[] args) {
-        
+
     }
 }
