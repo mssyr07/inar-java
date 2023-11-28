113f02cf329acc24d8d7a4a2bf8d7815b9fa0d70
mssyr07 <mssyr07@gmail.com>
2023-11-28 17:08:24 +0300

    updated

---
+++ src/week_12/assignments/Question_12_08/HexFormatException.java
@@ -0,0 +1,12 @@
+package week_12.assignments.Question_12_08;
+
+public class HexFormatException extends Exception {
+
+    public HexFormatException() {
+
+    }
+
+    public HexFormatException(String message) {
+        super(message);
+    }
+}
