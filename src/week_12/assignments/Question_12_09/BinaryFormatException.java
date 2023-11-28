7f5558393eed0b390ee9d2b5a45cf2546aa2ed22
mssyr07 <mssyr07@gmail.com>
2023-11-28 17:12:31 +0300

    updated

---
+++ src/week_12/assignments/Question_12_09/BinaryFormatException.java
@@ -0,0 +1,11 @@
+package week_12.assignments.Question_12_09;
+
+public class BinaryFormatException extends Exception {
+    BinaryFormatException() {
+
+    }
+
+    BinaryFormatException(String message) {
+        super(message);
+    }
+}
